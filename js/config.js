a2da4af86006f84561eb7a940e693eee62820cf1
Grant Gill <grantdgill@gmail.com>
2017-06-14 20:57:40 -0400

    initial commit

---
+++ js/config.js
@@ -0,0 +1,73 @@
+var fantasy = fantasy || {};
+fantasy.config = {
+	tournamentName:"US Open 2017",
+	tournamentId: 3066,
+	teams:[
+		{name: "JackG", golfers:[3448, 3470, 3702, 158, 388]},
+		{name: "Matt", golfers: [5467, 9780, 569, 110, 780]}
+		{name: "Buzz", golfers:[3448, 5467, 1680, 9780, 5860]},
+		{name: "Garrett", golfers:[3448, 5467, 388, 257, 1222]},
+		{name: "Jetta", golfers: [3448, 780, 1067, 3550, 6798]},
+		{name: "Michael", golfers:[3448, 5467, 1680, 9780, 3702]},
+		// {name: "DaveG", golfers:[1680, 388, 3702, 1293, 4304]},
+		{name: "JR", golfers:[3448, 1680, 569, 5860, 576]},
+		{name: "Richard", golfers:[3448, 5467, 9780, 3702, 388]},
+		// {name: "Richard2", golfers:[3448, 3470, 5467, 308, 9780, 569]},
+		{name: "Beth", golfers:[3448, 5467, 3702, 257, 3832]},
+		// {name: "Jim1", golfers:[1680, 388, 1293, 569, 5860]},
+		// {name: "Jim2", golfers:[1680, 5467, 308, 3702, 686]},
+		{name: "GrantG", golfers:[3448, 780, 1067, 3550, 6798]},
+		{name: "Rich", golfers:[1680, 3470, 158, 4848, 2552]},
+		// {name: "Rich2", golfers:[5467, 388, 5579, 257, 4848]},
+		{name: "Jim", golfers: [3448, 1680, 9780, 388, 576]},
+		{name: "Steve", golfers:[3448, 5467, 9780, 569, 1293]},
+		{name: "JohnG", golfers:[3448, 5467, 569, 257, 3670]},
+		{name: "DaveG", golfers: [3448, 3470, 9780, 780, 5548]}
+		// {name: "MattW", golfers:[5467, 3470, 388, 1293, 1097]},
+		// {name: "MarkT", golfers:[1680, 3702, 3448, 257, 774]},
+		// {name: "LauraB", golfers:[1293, 576, 3470, 1680, 388]}
+	],
+	firstPlaceValue: 50,
+	showCut: false,
+	apiKey: '_____'
+
+	// mcilroy - 3470
+	// spieth - 5467
+	// walker - 446
+	// fowler - 3702
+	// holmes - 1067
+	// mickelson - 308
+	// watson - 780
+	// johnson - 3448
+	// day - 1680
+	// tiger - 462
+	// scott - 388
+	// stenson - 576
+	// reed - 5579
+	// kuchar - 257
+	// snedeker - 1222
+	// rose - 569
+	// dufner - 110
+	// garcia - 158
+	// westwood - 455
+	// oosthuizen - 1293
+	// horschel - 1651
+	// matsuyama - 5860
+	// z. johnson - 686
+	// furyk - 153
+	// kirk - 3449
+	// conners - 9126
+	// shwartzel - 1097
+	// willet - 4304
+	// koepka - 6798
+	// kisner - 2552
+	// justin thomas - 4848
+	// bill haas - 774
+	// rahm - 9780
+	// leishman - 3351
+	// noren - 3832
+	// kaymer - 3670
+	// woodland - 3550
+
+
+};
